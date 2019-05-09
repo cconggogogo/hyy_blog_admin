ca0c58c38caefae3f7fa9ccd16ab740159aabc27
admin <15623552283@163.com>
2019-05-09 11:25:33 +0800

    上传初始化项目

---
+++ src/pages/User/Login.js
@@ -0,0 +1,105 @@
+import React, {Component} from 'react';
+import {connect} from 'dva';
+import {formatMessage, FormattedMessage} from 'umi/locale';
+import {Alert, Checkbox} from 'antd';
+import styles from './Login.less';
+import Login from "../../components/Login";
+
+const { Tab, UserName, Password, Submit } = Login;
+
+@connect(({ login, loading }) => ({
+  login,
+  submitting: loading.effects['login/login'],
+}))
+class LoginPage extends Component {
+  state = {
+    type: 'account',
+    autoLogin: true,
+  };
+
+  onTabChange = type => {
+    this.setState({ type });
+  };
+
+  onGetCaptcha = () =>
+    new Promise((resolve, reject) => {
+      this.loginForm.validateFields(['mobile'], {}, (err, values) => {
+        if (err) {
+          reject(err);
+        } else {
+          const { dispatch } = this.props;
+          dispatch({
+            type: 'login/getCaptcha',
+            payload: values.mobile,
+          })
+            .then(resolve)
+            .catch(reject);
+        }
+      });
+    });
+
+  handleSubmit = (err, values) => {
+    const { type } = this.state;
+    if (!err) {
+      const { dispatch } = this.props;
+      dispatch({
+        // type: 'login/loginAdmin',
+        type: 'login/login',
+        payload: {
+          ...values,
+          type,
+        },
+      });
+    }
+  };
+
+  changeAutoLogin = e => {
+    this.setState({
+      autoLogin: e.target.checked,
+    });
+  };
+
+  renderMessage = content => (
+    <Alert style={{ marginBottom: 24 }} message={content} type="error" showIcon />
+  );
+
+  render() {
+    const { login, submitting } = this.props;
+    const { type, autoLogin } = this.state;
+    return (
+      <div className={styles.main}>
+        <Login
+          defaultActiveKey={type}
+          onTabChange={this.onTabChange}
+          onSubmit={this.handleSubmit}
+          ref={form => {
+            this.loginForm = form;
+          }}
+        >
+          <Tab key="account" tab={formatMessage({ id: 'app.login.tab-login-credentials' })}>
+            {/*{login.status === 'error' &&*/}
+              {/*login.type === 'account' &&*/}
+              {/*!submitting &&*/}
+              {/*this.renderMessage('账户或密码错误（admin/888888）')}*/}
+            <UserName name="userName" placeholder="admin/user" />
+            <Password
+              name="password"
+              placeholder="888888/123456"
+              onPressEnter={() => this.loginForm.validateFields(this.handleSubmit)}
+            />
+          </Tab>
+          <div>
+            <Checkbox checked={autoLogin} onChange={this.changeAutoLogin}>
+              <FormattedMessage id="app.login.remember-me" />
+            </Checkbox>
+          </div>
+          <Submit loading={submitting}>
+            <FormattedMessage id="app.login.login" />
+          </Submit>
+        </Login>
+      </div>
+    );
+  }
+}
+
+export default LoginPage;
