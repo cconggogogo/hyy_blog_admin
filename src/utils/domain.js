11b090cfcd028bfe0abe9b26672ec8a12b44f1c5
admin <15623552283@163.com>
2019-05-10 16:11:05 +0800

    modify

--- src/utils/domain.js
+++ src/utils/domain.js
@@ -1,4 +1,4 @@
-let domain = 'http://huyangyang.cn/'; // 正式域名
+let domain = 'http://www.huyangyang.cn:81/'; // 正式域名
 if (process.env.NODE_ENV === 'development') {
   // 开发环境下，本地地址
   domain = 'http://localhost:3001/';
